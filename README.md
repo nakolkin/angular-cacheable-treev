547260659bdbea7215a721f23b2f137f514fedf5
Nikolaenko, Artem <Artem.Nikolaenko@neustar.biz>
2016-02-01 17:51:45 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# angular-cacheable-treev
+AngularJS 1.x Cacheable Tree View directive. 
+================
+
+Most suitable for tree-like menus.
+
+[![ScreenShot](https://github.com/nakolkin/angular-cacheable-treeview/raw/master/img/preview.png)]
+
+## Featuers and limitations:
+   - Fast DOM genereation, it tooks about 15 ms to genereate list of 1185 items on my MacBook Pro late 2013,
+     it is about 100-times faster compared to Angular Tree View directive which is implemented with ng-repeate.
+   - It is possible to cache the resulted DOM by adding the 'rememberMe' flag to the first node in the model.
+   - Watches only for model instance changes, which means it doesn't update the DOM if changes are inside the tree model.
+   - Handles node selection by setting scope[treeModel].currentNode and adding 'selected' class to the selected element.
+
+## Installation
+
+...
+
+## Usage
+
+Attributes of angular cacheable treeview are below.
+
+- angular-treeview: the treeview directive.
+- tree-id: unique tree-id to store currentNode in the current $scope
+- tree-model: array of nodes, the tree model on $scope.
+
+Here is a simple example.
+
+
+```html
+<div
+    data-cacheable-treeview="true"
+	data-tree-id="treeId"
+	data-tree-model="treeModel"
+ >
+</div>
+```
+
+Example model:
+
+```javascript
+$scope.treeModel = 
+[
+	{ "name" : "User", "id" : 1, "children" : [
+		{ "name" : "subUser1", "id" : 2, "children" : [] },
+		{ "name" : "subUser2", "id" : 3, "children" : [
+			{ "name" : "subUser2-1", "id" : 4, "children" : [
+				{ "name" : "subUser2-1-1", "id" : 5, "children" : [] },
+				{ "name" : "subUser2-1-2", "id" : 6, "children" : [] }
+			]}
+		]}
+	]},
+	{ "name" : "Admin", "id" : 7, "children" : [] },
+	{ "name" : "Guest", "id" : 8, "children" : [] }
+];	 
+```
+
+## Selection
+
+If tree node is selected, then that selected tree node is saved to $scope."TREE ID".currentNode. By using $watch, the controller can recognize the tree selection.
+
+
+```javascript
+$scope.$watch( 'treeId.currentNode', function( newObj, oldObj ) {
+     console.log( 'Node Selected!!' );
+     console.log( $scope.treeId.currentNode );    }
+});
+```
+
+## Examples
+
+
+## License
+
+The MIT License.
+
+See [LICENSE](https://github.com/eu81273/angular.treeview/blob/master/LICENSE)
---
+++ angular-cacheable-treeview.js
@@ -0,0 +1,152 @@
+(function() {
+  'use strict';
+
+  /**
+   * AngularJS 1.x Cacheable Tree View directive. Most suitable for tree-like menus.
+   *
+   * Featuers and limitations:
+   *  - Fast DOM genereation, it tooks about 15 ms to genereate list of 1185 items on my MacBook Pro late 2013,
+   *    it is about 100-times faster compared to Angular Tree View directive which is implemented with ng-repeate.
+   *  - It is possible to cache the resulted DOM by adding the 'rememberMe' flag to the first node in the model.
+   *  - Watches only for model instance changes, which means it doesn't update the DOM if changes are inside the tree model.
+   *  - Handles node selection by setting scope[treeModel].currentNode and adding 'selected' class to the selected element.
+   *
+   * Input attributes:
+   * @treeId - unique tree-id to store currentNode in the current $scope
+   * @treeModel - array of nodes, the tree model on $scope.
+   *
+   * Usage:
+   *
+   * <pre>
+   *  <code>
+   *   &lt div data-cacheable-treeview
+   *           data-tree-id="tree"
+   *           data-tree-model="roleList"
+   *     &lt/div>
+   *  </code>
+   * </pre>
+   *
+   * Pre requirements:
+   * - depends on lodash
+   * - every node object should contain fields: int id, String name and children array;
+   *
+   */
+
+  angular.module('cacheableTreeview', [])
+    .directive('treeModel', angularTreeviewDirective);
+
+  function angularTreeviewDirective($compile) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var treeId      = attrs.treeId;
+        var treeModel   = attrs.treeModel
+        var rememberDOM = _.memoize(printDOM, hashcodeTreeIds)
+        var selectedElement;
+
+        scope[treeId] = scope[treeId] || {};
+
+        scope.$watch(treeModel, buildTree);
+
+        function buildTree(tree) {
+          console.time('buildTree')
+          if (!_.isEmpty(tree)) {
+            element.html('').append(tree[0].rememberMe ? rememberDOM(tree) : printDOM(tree));
+            element.on('click', onTreeClick);
+          }
+          console.timeEnd('buildTree')
+        }
+
+        function printDOM(tree) {
+          if (!_.isEmpty(tree)) {
+            return '<ul>' + _.reduce(tree, printDOMReducer, '') + '</ul>';
+          } else {
+            return '';
+          }
+        }
+
+        function printDOMReducer(result, node) {
+          var isSelected = scope[treeId].currentNode && _.eq(node.id, scope[treeId].currentNode.id);
+          return [result, '<li><span data-id="' + node.id + '"', isSelected ? 'class="selected"' : '', '>', node.name, '</span>', printDOM(node.children), '</li>'].join('');
+        }
+
+        /**
+         * Instead of creating separate click-handler on each node, which is much slower,
+         * and due to the tree DOM lost all events handlers when it moved out of page, so we can't simply restore it from cache,
+         * we are listenning for whole tree and filter out events we are not interested in.
+         */
+        function onTreeClick(e) {
+          if (e.target.tagName === "SPAN") { // we are interested only on node clicks
+            if (selectedElement) {
+              selectedElement.className = '';
+            }
+            selectedElement                = e.target;
+            selectedElement.className      = 'selected';
+            scope[treeId].currentNode      = {};
+            scope[treeId].currentNode.id   = parseInt(e.target.getAttribute('data-id'));
+            scope[treeId].currentNode.name = e.target.innerHTML;
+            scope.$digest();
+          }
+        }
+
+        function hashcodeTreeIds(tree) {
+          if (_.isEmpty(tree)) {
+            return 0
+          } else {
+            return _.reduce(tree, function(result, node) {
+              return ((( node.id * 31 + result) % 7177) * 31 + hashcodeTreeIds(node.children)) % 7177
+            }, 0);
+          }
+        }
+
+/*      Little bit slower implementation, also we are lost event handlers here after removing a tree from the page
+
+        function onNodeClick(node) {
+          return function(e) {
+            if (selectedElement) {
+              selectedElement.className = '';
+            }
+            selectedElement = e.target;
+            selectedElement.className = 'selected';
+            scope[treeId].currentNode = node;
+            scope.$digest();
+          }
+        }
+
+        function buildDOMReducer(result, node) {
+          var leaf = angular.element('<li><span>' + node.name + '</span></li>');
+          var span = leaf.children().eq(0)
+          span.on('click', onNodeClick(node))
+          if (scope[treeId].currentNode && _.eq(node.id, scope[treeId].currentNode.id)) {
+            span.addClass('selected');
+          }
+          return result.append(leaf.append(buildDOM(node.children)))
+        }
+
+        function buildDOM(tree) {
+          if (!_.isEmpty(tree)) {
+            return _.reduce(tree, buildDOMReducer, angular.element('<ul></ul>'));
+          } else {
+            return angular.element('');
+          }
+        }
+
+        function concatTreeIds(tree) {
+          return _.reduce(tree, function(result, node) {
+            return result + node.id + concatTreeIds(node.children);
+          }, '')
+        }
+
+        function sumTreeIds(tree) {
+          return _.reduce(tree, function(result, node) {
+            return result + parseInt(node.id, 0) + sumTreeIds(node.children);
+          }, 0);
+        }
+*/
+
+      }
+
+    };
+  }
+
+})();
